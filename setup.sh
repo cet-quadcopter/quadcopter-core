534704a637596dcdf391a13009fd49ac88a100a1
Sreejith Krishnan R <sreejith.aruvikkara@gmail.com>
2019-01-16 12:29:45 +0530

    Make platform agnostic

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+if [ ! $ROS_PACKAGE_PATH ] || [ ! -d $ROS_PACKAGE_PATH ]; then
+  echo "ROS must be installed"
+  exit 0
+fi
+
+if [ ! -f ./CMakeLists.txt ]; then
+  ln -s $ROS_PACKAGE_PATH/catkin/cmake/toplevel.cmake CMakeLists.txt
+fi
+
 pushd rosdeps
 sh setup.sh
 popd
