31da7c0389fd03fbf979ca23663f5c9e6debf644
Sreejith Krishnan R <sreejith.aruvikkara@gmail.com>
2019-04-06 00:27:59 +0530

    Add base falcon node

---
+++ falcon/src/falcon_node.cpp
@@ -0,0 +1,73 @@
+#include "ros/ros.h"
+
+#include "spdlog/spdlog.h"
+#include "Eigen/Dense"
+#include "falcon/state/manager.h"
+#include "drone_constants/FLIGHT_CONTROL.h"
+#include "drone_constants/POSITION_CONTROL.h"
+#include "drone_std_msgs/Accelerometer.h"
+#include "drone_std_msgs/Gyro.h"
+#include "drone_std_msgs/Compass.h"
+#include "drone_std_msgs/GPS.h"
+
+
+using namespace falcon::state;
+using namespace drone_constants;
+using namespace drone_std_msgs;
+using namespace Eigen;
+
+
+class FalconROS {
+  private:
+  StateManager state_manager_;
+
+  public:
+  FalconROS(AttitudeSensorParams attitude_params, VelocitySensorParams velocity_params)
+  : state_manager_(attitude_params, velocity_params, ros::Time::now().toSec()) {}
+
+  void HandleAccelerometerMessage(const Accelerometer::ConstPtr& msg) {
+    state_manager_.PostAccelerometer(Vector3f(msg->ax, msg->ay, -msg->az));
+  }
+
+  void HandleGyroMessage(const Gyro& msg) {
+    state_manager_.PostGyro(Vector3f(msg.gx, msg.gy, -msg.gz));
+  }
+
+  void HandleMagnetometerMessage(const Compass& msg) {
+    state_manager_.PostMagnetometer(Vector3f(msg.mx, msg.my, -msg.mz));
+  }
+
+  void HandleGPSMessage(const GPS& msg) {
+    state_manager_.PostGPSVelocity(Vector3f(msg.vn, msg.ve, msg.vd));
+    state_manager_.SpinOnce(ros::Time::now().toSec());
+  }
+};
+
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "falcon");
+  ros::NodeHandle node;
+
+  auto attitude_sensor_params = AttitudeSensorParams {
+    .accelerometer_gain = 0.2,
+    .magnetometer_gain = 0.5,
+    .covariance_accelerometer = Matrix3f::Identity() * 0.0001,
+    .covariance_magnetometer = Matrix3f::Identity() * 0.0001,
+    .covariance_gyro = Matrix3f::Identity() * 0.0001
+  };
+
+  auto velocity_sensor_params = VelocitySensorParams {
+    .alpha_linear_velocity = Vector3f(0.2, 0.2, 0.2),
+    .alpha_angular_velocity = Vector3f(0.2, 0.2, 0.2)
+  };
+
+  auto falcon = FalconROS(attitude_sensor_params, velocity_sensor_params);
+
+  auto sub_acc = node.subscribe(FLIGHT_CONTROL::TOPIC_ACCELEROMETER, 10, &FalconROS::HandleAccelerometerMessage, &falcon);
+  auto sub_gyro = node.subscribe(FLIGHT_CONTROL::TOPIC_GYRO, 10, &FalconROS::HandleGyroMessage, &falcon);
+  auto sub_compass = node.subscribe(FLIGHT_CONTROL::TOPIC_COMPASS, 10, &FalconROS::HandleMagnetometerMessage, &falcon);
+  auto sub_gps = node.subscribe(POSITION_CONTROL::TOPIC_GPS, 10, &FalconROS::HandleGPSMessage, &falcon);
+
+  ros::spin();
+  return 0;
+}
