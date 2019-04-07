5370e77335bb940eb345f1c4f82342fd6f386cf4
Sreejith Krishnan R <sreejith.aruvikkara@gmail.com>
2019-04-07 10:29:25 +0530

    Change body coordinate system alignment

--- falcon/src/falcon_node.cpp
+++ falcon/src/falcon_node.cpp
@@ -38,15 +38,15 @@ class FalconROS {
     control_(control_params, ros::Time::now().toSec()), pub_control_signal_(pub_control_signal) {}
 
   void HandleAccelerometerMessage(const Accelerometer& msg) {
-    state_manager_.PostAccelerometer(Vector3f(-msg.ax, msg.ay, -msg.az));
+    state_manager_.PostAccelerometer(Vector3f(msg.ax, -msg.ay, -msg.az));
   }
 
   void HandleGyroMessage(const Gyro& msg) {
-    state_manager_.PostGyro(Vector3f(-msg.gx, msg.gy, -msg.gz));
+    state_manager_.PostGyro(Vector3f(msg.gx, -msg.gy, -msg.gz));
   }
 
   void HandleMagnetometerMessage(const Compass& msg) {
-    state_manager_.PostMagnetometer(Vector3f(-msg.mx, msg.my, -msg.mz));
+    state_manager_.PostMagnetometer(Vector3f(msg.mx, -msg.my, -msg.mz));
     state_manager_.SpinOnce(ros::Time::now().toSec());
 
     auto control = control_.GetControlSignal(state_manager_, ros::Time::now().toSec());
