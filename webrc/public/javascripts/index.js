615a221691bb6b2285283f9d403562233bf34d51
Sreejith Krishnan R <sreejith.aruvikkara@gmail.com>
2019-04-09 22:25:07 +0530

    Add joystick

---
+++ webrc/public/javascripts/index.js
@@ -0,0 +1,15 @@
+var joystickLeft = nipplejs.create({
+  zone: document.getElementById("jLeft"),
+  mode: "static",
+  size:180,
+  position: { left: '30%', top: '50%' },
+  color: "red"
+});
+
+var joystickRight = nipplejs.create({
+  zone: document.getElementById("jRight"),
+  mode: "static",
+  size:180,
+  position: { left: '70%', top: '50%' },
+  color: "green"
+});
