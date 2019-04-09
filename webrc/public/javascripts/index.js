92dc84fad424a69b8f60cf1de4d8fa458074f2a2
Sreejith Krishnan R <sreejith.aruvikkara@gmail.com>
2019-04-09 22:33:23 +0530

    Init controls for vn ve vd and omega_d

--- webrc/public/javascripts/index.js
+++ webrc/public/javascripts/index.js
@@ -2,14 +2,24 @@ var joystickLeft = nipplejs.create({
   zone: document.getElementById("jLeft"),
   mode: "static",
   size:180,
-  position: { left: '30%', top: '50%' },
+  position: { left: '50%', top: '50%' },
   color: "red"
+});
+
+var joystickCenter = nipplejs.create({
+  zone: document.getElementById("jCenter"),
+  mode: "static",
+  size:180,
+  position: { left: '50%', top: '50%' },
+  lockY: true,
+  color: "blue"
 });
 
 var joystickRight = nipplejs.create({
   zone: document.getElementById("jRight"),
   mode: "static",
   size:180,
-  position: { left: '70%', top: '50%' },
+  position: { left: '50%', top: '50%' },
+  lockX: true,
   color: "green"
 });
