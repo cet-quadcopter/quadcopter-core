2079f832010fcbce440ab58d4ee0fca6b9ec8260
Sreejith Krishnan R <sreejith.aruvikkara@gmail.com>
2019-04-09 23:50:11 +0530

    Webrc raw implementation

--- webrc/app.js
+++ webrc/app.js
@@ -1,20 +1,48 @@
+var rosnode = require('rosnodejs');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+rosnode.initNode('webrc');
+
+var drone_msgs = rosnode.require('drone_std_msgs');
+const pub = rosnode.nh.advertise('/controls/velocity', 'drone_std_msgs/InputVelocity');
+
+var WebSocketServer = require('ws').Server,
+wss = new WebSocketServer({port: 40510});
+
+var state = {
+  v_n: 0.0,
+  v_e: 0.0,
+  v_d: 0.0,
+  omega_d: 0.0
+}
+wss.on('connection', function (ws) {
+  ws.on('message', function (message) {
+    var signal = JSON.parse(message);
+    if (signal.type == 'vh') {
+      state.v_n = signal.signal.x * 3;
+      state.v_e = signal.signal.y * 3;
+    } else if (signal.type == 'vd') {
+      state.v_d = signal.signal * 3;
+    } else if (signal.type == 'orientation') {
+      state.omega_d = signal.signal * 0.2;
+    }
+
+    var msg = new drone_msgs.msg.InputVelocity(state);
+    pub.publish(msg);
+  });
+});
 
 module.exports = app;
--- webrc/public/javascripts/index.js
+++ webrc/public/javascripts/index.js
@@ -1,25 +1,83 @@
+const href = window.location.href;
+const socket = new WebSocket('ws://192.168.43.173:40510/');
+
 var joystickLeft = nipplejs.create({
   zone: document.getElementById("jLeft"),
   mode: "static",
+  restJoystick: true,
   size:180,
   position: { left: '50%', top: '50%' },
   color: "red"
 });
 
+
 var joystickCenter = nipplejs.create({
   zone: document.getElementById("jCenter"),
   mode: "static",
   size:180,
+  restJoystick: true,
   position: { left: '50%', top: '50%' },
   lockY: true,
   color: "blue"
 });
 
+
 var joystickRight = nipplejs.create({
   zone: document.getElementById("jRight"),
   mode: "static",
   size:180,
+  restJoystick: true,
   position: { left: '50%', top: '50%' },
   lockX: true,
   color: "green"
 });
+
+
+joystickLeft.on('move end', function (evt, nip) {
+  let distance = 0;
+  let angle = 0;
+
+  if (evt.type == 'move') {
+    distance = nip.distance;
+    angle = nip.angle.radian;
+  }
+
+  const x = distance * Math.cos(angle) / 90;
+  const y = distance * Math.sin(angle) / 90;
+
+  sendSignal('vh', {x: y, y: x});
+});
+
+
+joystickCenter.on('move end', function (evt, nip) {
+  let distance = 0;
+  let angle = 0;
+
+  if (evt.type == 'move') {
+    distance = nip.distance;
+    angle = nip.angle.radian;
+  }
+
+  sendSignal('vd', -distance * Math.sin(angle) / 90);
+});
+
+
+joystickRight.on('move end', function (evt, nip) {
+  let distance = 0;
+  let angle = 0;
+
+  if (evt.type == 'move') {
+    distance = nip.distance;
+    angle = nip.angle.radian;
+  }
+
+  sendSignal('orientation', -distance * Math.cos(angle) / 90);
+});
+
+
+function sendSignal(type, signal) {
+  socket.send(JSON.stringify({
+    type: type,
+    signal: signal
+  }));
+}
